4b7d7b5dac6ce1207a4ab7b973e562e83c2ff198
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-28 19:31:35 +0100

    Configured AbstratRabbitMQ

--- Order.api/Program.cs
+++ Order.api/Program.cs
@@ -1,8 +1,32 @@
+using AbstractedRabbitMQ.Constants;
+using AbstractedRabbitMQ.Setup;
+using Order.api.DataAcess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSqlServer<OrderDbContext>(builder.Configuration.GetConnectionString("connString"));
+
+builder.Services.AddRabbitMQConnection(x =>
+{
+    x.Url = "amqp://guest:guest@localhost:5672";
+    x.ClientProvideName = "Order web Api";
+});
+
+builder.Services.AddRabbitMQPublisher(x =>
+{
+    x.exchangeType = ExchangeTypeRMQ.direct;
+    x.exchange = "order-exchange";
+});
+builder.Services.AddRabbitMQSubscriber(x =>
+{
+    x.exchange = "invetory-exchange";
+    x.exchangeType= ExchangeTypeRMQ.direct;
+    x.queue = "invetory-response";
+    x.routingKey = "inventory.updated";
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
