45d93b11eca1c2678ae5f49be043c269114218af
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-29 04:11:16 +0100

    Added order models

---
+++ Order.api/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.api.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ Order.api/Models/OrderDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace Order.api.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+        public Order Order { get; set; }
+    }
+}
