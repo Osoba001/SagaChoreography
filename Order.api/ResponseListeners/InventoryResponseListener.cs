7c94ce57e47fbbce5ad831115def9f429ead2263
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-29 04:15:31 +0100

    Created Inventory Response Listener which implemented IHostedService

---
+++ Order.api/ResponseListeners/InventoryResponseListener.cs
@@ -0,0 +1,51 @@
+﻿using AbstractedRabbitMQ;
+using AbstractedRabbitMQ.Subscribers;
+using Order.api.DataAcess;
+using SagaModel.Orders;
+
+namespace Order.api.ResponseListeners
+{
+    public class InventoryResponseListener:IHostedService
+    {
+        private readonly OrderDbContext _context;
+        private readonly ISubscriber _subscriber;
+        private readonly ILogger<InventoryResponseListener> log;
+
+        public InventoryResponseListener(OrderDbContext context,ISubscriber subscriber, ILogger<InventoryResponseListener> log)
+        {
+            _context = context;
+            _subscriber = subscriber;
+            this.log = log;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _subscriber.SubscribeAsyc<InventoryResponse>(Subscribe);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private async Task<bool> Subscribe(SubResult<InventoryResponse> response,IDictionary<string,object> header)
+        {
+            if(!response.IsSuccess)
+            {
+                log.LogError(response.ReasonPhrase);
+                return false;
+            }
+            else
+            {
+                if (!response.Value!.IsSuccess)
+                {
+                    var orderDetail = _context.OderDetail.Find(response.Value.OrderId);
+                    _context.OderDetail.Remove(orderDetail);
+                    await _context.SaveChangesAsync();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
