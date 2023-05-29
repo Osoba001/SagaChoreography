79d1d7625b12f0d6f5b4fe853fab036b77d433e5
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-29 04:07:49 +0100

    Created inventory data access layer with efcore

---
+++ Inventory.mvc/DataAccess/InventoryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Inventory.mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.mvc.DataAccess
+{
+    public class InventoryDbContext:DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options):base(options) { }
+
+        public DbSet<InventoryModel> Inventory { get; set; }
+    }
+}
