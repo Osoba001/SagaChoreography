071f963f7d94c8f7169a5bfc855d86ac94d76693
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-05-29 04:08:49 +0100

    Created Order access layer with efcore

---
+++ Order.api/DataAcess/OrderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.api.Models;
+
+namespace Order.api.DataAcess
+{
+    public class OrderDbContext:DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options):base(options)
+        {
+           
+        }
+
+        public DbSet<Models.Order> Oder { get; set; }
+        public DbSet<OrderDetail> OderDetail { get; set; }
+    }
+}
